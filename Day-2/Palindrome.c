2acf84a31f03334e9cc4c8abdaf68ef38401d5ff
TryEye <tb1093612@gmail.com>
2026-06-10 17:39:34 +0530

    Question 8

---
+++ Day-2/Palindrome.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() 
+{
+    int n, reversed = 0, digit;
+
+    printf("Enter an integer: ");
+    scanf("%d", &n);
+
+    int original = n;
+    int temp = n;
+
+    if (temp < 0) 
+    {
+        temp = -temp;
+    }
+
+    while (temp != 0) 
+    {
+        digit = temp % 10;
+        reversed = reversed * 10 + digit;
+        temp /= 10;
+    }
+
+    if (original == reversed) 
+    {
+        printf("%d is a Palindrome number.\n", original);
+    } else {
+        printf("%d is NOT a Palindrome number.\n", original);
+    }
+
+    return 0;
+}
